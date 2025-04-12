ecc675cdb4d46d0f7be4d9431b46c649cc276f3e
Velial-Re <defflldd@gmail.com>
2025-04-12 21:50:39 +0600

    refactor: replace fetch with api.jc client

---
+++ Frontend/src/api/api.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: "http://localhost:8080/api",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+})
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem("token");
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+api.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                await api.post("/auth/refresh", {}, {
+                    withCredentials: true
+                })
+                return api(originalRequest);
+            } catch (refreshError) {
+                if (refreshError.response?.status === 401) {
+                    window.location.href = "/auth/login";
+                }
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+)
+export default api;
