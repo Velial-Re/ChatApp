865c38b4ec011a69c53a7eb7b6ff352fe94b072c
Velial-Re <defflldd@gmail.com>
2025-04-13 19:41:44 +0600

    feat: add ChatPage.jsx to repository

--- Frontend/src/pages/ChatPage/ChatPage.jsx
+++ Frontend/src/pages/ChatPage/ChatPage.jsx
@@ -1,7 +1,8 @@
 import {useParams} from 'react-router-dom'
-import {Chat} from "../Chat/Chat.jsx";
-import {useChat} from "../../../context/ChatContext.jsx";
+import {Chat} from "../../components/chat/Chat/Chat.jsx";
+import {useChat} from "../../context/ChatContext.jsx";
 import {useEffect} from "react";
+import {useAuth} from "../../context/AuthContext.jsx";
 
 export default function ChatPage() {
     const {chatName} = useParams()
@@ -11,7 +12,7 @@ export default function ChatPage() {
         if (user && chatName && chatName !== chatRoom) {
             joinChat(chatName);
         }
-    }, [chatName, joinChat, chatRoom]);
+    }, [chatName, joinChat, chatRoom, user]);
     return (
         <div className="chat-page">
             {chatName === chatRoom ? (
