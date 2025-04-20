1f8112d411d3688f753dfd430fc97da74a745285
vwency <duckdodo666@gmail.com>
2025-04-20 23:36:58 +0600

    feat: init redux

---
+++ Frontend/src/store/auth/authActions.js
@@ -0,0 +1,57 @@
+import { setUser, setLoading, setError } from './authSlice'
+import api from '../../api/api'
+
+// Экшены
+export const loginAction = (userData) => async (dispatch) => {
+  try {
+    dispatch(setLoading(true))
+    const response = await api.post('auth/login', {
+      username: userData.username,
+      password: userData.password,
+    })
+    dispatch(setUser({ username: response.data.Username }))
+    dispatch(setLoading(false))
+  } catch (error) {
+    dispatch(setLoading(false))
+    dispatch(setError(error.response?.data?.message || 'Login failed'))
+    console.error('Login failed', error)
+  }
+}
+
+export const logoutAction = () => async (dispatch) => {
+  try {
+    dispatch(setLoading(true))
+    await api.post('auth/logout')
+    dispatch(setUser(null))
+    dispatch(setLoading(false))
+  } catch (error) {
+    dispatch(setLoading(false))
+    dispatch(setError(error.message))
+    console.error('Logout failed', error)
+  }
+}
+
+export const fetchUserAction = () => async (dispatch) => {
+  try {
+    dispatch(setLoading(true))
+    const response = await api.get('auth/user')
+    dispatch(setUser(response.data))
+    dispatch(setLoading(false))
+  } catch (error) {
+    dispatch(setLoading(false))
+    dispatch(setError(error.message))
+    console.error('Fetch user failed', error)
+  }
+}
+
+export const refreshAuthAction = () => async (dispatch) => {
+  try {
+    dispatch(setLoading(true))
+    await api.post('auth/refresh')
+    dispatch(setLoading(false))
+  } catch (error) {
+    dispatch(setLoading(false))
+    dispatch(setError(error.message))
+    console.error('Refresh failed', error)
+  }
+}
