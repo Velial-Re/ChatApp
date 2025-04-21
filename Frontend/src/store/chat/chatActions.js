25986cc5de2bdc41df09ac1e4524a70383a71c3e
vwency <duckdodo666@gmail.com>
2025-04-21 22:43:57 +0600

    chore: dulicates removed redux selector

--- Frontend/src/store/chat/chatActions.js
+++ Frontend/src/store/chat/chatActions.js
@@ -21,13 +21,3 @@ export const joinChatAction = (chatName) => (dispatch) => {
 }
 
 export { createChat }
-
-export const selectChatState = (state) => state.chat
-export const selectMessages = (state) => state.chat.messages
-export const selectUserChats = (state) => state.chat.userChats
-export const selectCurrentRoom = (state) => state.chat.chatRoom
-export const selectChatLoading = (state) => state.chat.isChatLoading
-export const selectChatConnection = (state) => state.chat.isConnected
-export const selectShowCreateModal = (state) => state.chat.showCreateModal
-export const selectShowJoinModal = (state) => state.chat.showJoinModal
-export const selectNewChatName = (state) => state.chat.newChatName
